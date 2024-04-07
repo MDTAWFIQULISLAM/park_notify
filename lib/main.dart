036ce1d23a7a42bb847ad149f99158c4df8d56b0
Maimuna <2023538@brunel.ac.uk>
2024-04-07 15:07:34 +0100

    Added necessary dependencies and google json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/scheduler.dart';
 import 'core/app_export.dart';
 
+
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);

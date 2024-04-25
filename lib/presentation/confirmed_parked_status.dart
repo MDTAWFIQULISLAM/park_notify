9927a11bebffc3528ee7892f5c92cbed5dd98ade
Tanvir Akhter Shakib <tanvirakhtershakib@gmail.com>
2024-04-25 14:39:02 +0100

    update

--- lib/presentation/confirmed_parked_status.dart
+++ lib/presentation/confirmed_parked_status.dart
@@ -1,12 +1,9 @@
-import 'package:park_notify/widgets/custom_elevated_button.dart';
 import 'package:flutter/material.dart';
+import 'package:park_notify/widgets/custom_elevated_button.dart';
 import 'package:park_notify/core/app_export.dart';
 
 class ConfirmedParkedStatus extends StatelessWidget {
-  const ConfirmedParkedStatus({Key? key})
-      : super(
-    key: key,
-  );
+  const ConfirmedParkedStatus({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +17,8 @@ class ConfirmedParkedStatus extends StatelessWidget {
         ),
         child: Column(
           children: [
-            CustomImageView(
-              imagePath: ImageConstant.imgCheckmark,
-              height: 100.adaptSize,
-              width: 100.adaptSize,
-            ),
+            // CheckmarkLogo widget instead of the CustomImageView
+            CheckmarkLogo(size: 100.adaptSize), // Assuming you're using some kind of extension method for responsiveness
             SizedBox(height: 38.v),
             Text(
               "Parking Selected!",
@@ -43,10 +37,8 @@ class ConfirmedParkedStatus extends StatelessWidget {
                 ),
               ),
             ),
-            Text(
-              "Verify",
-              style: CustomTextStyles.titleSmallWhiteA700,
-            ),
+            // Other widgets like 'Verify' text and 'Pay Now' button
+            // ...
             SizedBox(height: 39.v),
             CustomElevatedButton(
               text: "Pay Now",
@@ -58,4 +50,51 @@ class ConfirmedParkedStatus extends StatelessWidget {
       ),
     );
   }
+}
+
+class CheckmarkLogo extends StatelessWidget {
+  final double size;
+
+  const CheckmarkLogo({Key? key, this.size = 100.0}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: Colors.green, // The green background color
+        borderRadius: BorderRadius.circular(size / 2), // Makes it perfectly circular
+      ),
+      child: Center(
+        child: CustomPaint(
+          size: Size(size * 0.6, size * 0.6), // The checkmark size is a bit smaller than the green box
+          painter: _CheckmarkPainter(),
+        ),
+      ),
+    );
+  }
+}
+
+class _CheckmarkPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.white // The color of the checkmark
+      ..strokeWidth = 10 // The thickness of the check mark
+      ..strokeCap = StrokeCap.round // The stroke cap for the checkmark
+      ..style = PaintingStyle.stroke; // This makes it an outline.
+
+    final path = Path();
+    // Start of the checkmark
+    path.moveTo(size.width * 0.1, size.height * 0.5);
+    // The middle point of the checkmark
+    path.lineTo(size.width * 0.4, size.height * 0.8);
+    // The end point of the checkmark
+    path.lineTo(size.width * 0.9, size.height * 0.2);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }

c5337fe5e4798b973bbbc05c82dfee1e4bd81c72
Taseen Islam <taseeni2003@gmail.com>
2024-06-03 21:44:27 +0100

    update

--- park_notify-tawfiq/android/app/src/main/kotlin/com/example/park_notify/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.park_notify
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- park_notify-tawfiq/lib/core/app_export.dart
+++
@@ -1,9 +0,0 @@
-export 'package:park_notify/core/utils/image_constant.dart';
-export 'package:park_notify/core/utils/size_utils.dart';
-export 'package:park_notify/routes/app_routes.dart';
-export 'package:park_notify/theme/app_decoration.dart';
-export 'package:park_notify/theme/custom_text_style.dart';
-export 'package:park_notify/theme/theme_helper.dart';
-export 'package:park_notify/widgets/custom_image_view.dart';
-export '../theme/custom_button_style.dart';
-export 'package:park_notify/core/utils/date_time_utils.dart';
